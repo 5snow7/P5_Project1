993abb20657a8d181ddf7970d0ba541c59ab14cf
mark <snowtowind@yahoo.com>
2018-12-16 14:46:45 -0800

    added arrrays

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
 let can;
-let p1,p2;
+let p1,p2;let parray=[];
 function setup(){
+	extraS();
 	can=createCanvas(800,600);can.position(50,50);
 	can.class("bdd");this.spx=4;this.spy=3;
 	p1=new pars(200,200,"testOne","id1");
 	p2=new pars(400,200,"testTwo","id2");
-}
+parray.push(p1);parray.push(new pars(100,100,"2cd array element","id3"));
+
+	}
 
 function draw(){
 background(255,0,0);
-p1.move();p1.bdd();
+//p1.move();p1.bdd();
 p2.move();p2.bdd();
-
+for(let k=0;k<parray.length;k++)
+{
+parray[k].move();
+parray[k].bdd();
+if(parray[k].count>4){parray.splice(1,k);}
+}
 }	
 
--- testClass.js
+++ testClass.js
@@ -2,6 +2,7 @@
 
 class pars{
 constructor(x,y,wd,idd){
+this.count=0;
 this.p=createP(wd);this.x=x;this.y=y;
 this.spx=4;this.spy=3;
 this.p.position(x,y);this.p.id(idd);
@@ -19,7 +20,8 @@ move(){
 	}
 
 bdd(){
-	if(this.x>windowWidth||this.x<0){this.spx=this.spx*(-1);}
+	if(this.x>windowWidth||this.x<0){this.count++;
+	this.spx=this.spx*(-1);}
     if(this.y>windowHeight||this.y<0){this.spy=this.spy*(-1);}
 	}
 
